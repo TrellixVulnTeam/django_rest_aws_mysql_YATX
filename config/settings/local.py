ca1eed2c9bf28f2102671630b88e9b5fbe222d49
ksb <ksoonb85@gmail.com>
2021-10-19 10:52:49 +0900

    add mysql

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_rest_test',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
