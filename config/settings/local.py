63f8c3a90fa7db0a24647792a4e4205363331f14
ksb <ksoonb85@gmail.com>
2021-10-26 13:24:16 +0900

    modify setting file

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_rest_test',
-        'USER': get_secret("LOCAL_DB_USER"),
+        'USER': get_secret("LOCAL_DB_USR"),
         'PASSWORD': get_secret("LOCAL_DB_PWD"),
         'HOST': 'localhost',
         'PORT': '3306',
